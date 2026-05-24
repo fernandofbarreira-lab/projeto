7753554d7bd338dc4ec704f5825d64c775cb65bd
fernandofbarreira-lab <fernandofbarreira@gmail.com>
2026-05-24 19:42:20 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+   {
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal, a Fadinha, é uma das maiores skatistas do mundo na modalidade street. Fenômeno de popularidade e carisma, ela conquistou o mundo ao se tornar a medalhista olímpica mais jovem do Brasil e segue colecionando títulos mundiais com seu estilo único e inspirador.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+},
+{
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }, 
+    
+];
+
